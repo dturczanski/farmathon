20a3459847671fc81d5328db9426be0b463ad19b
Łukasz Młynik <requiemos@gmail.com>
2012-12-18 16:50:15 +0100

    Added new enquiry search form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,19 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index())
-  }
+   val enquirySearchForm = Form(
+    tuple(
+      "surname" -> nonEmptyText,
+      "enquiryId" -> nonEmptyText
+    )
+  )  
   
+  def index = Action {
+    Ok(views.html.index(enquirySearchForm))
+  }  
 }
