9820050bcb3b393ecb03da0a233c83b440ff7d97
rafal <rkondr@gmail.com>
2012-12-18 14:07:34 +0100

    added Enquiries controller scaffold

---
+++ app/controllers/Enquiries.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Enquiries extends Controller {
+  // display list of enquiries
+  def index = TODO
+
+  // display enquiry details
+  def details(id: Long) = TODO
+
+  // accept enquiry
+  def accept(id: Long) = TODO
+
+  // decline enquiry
+  def decline(id: Long) = TODO
+}
