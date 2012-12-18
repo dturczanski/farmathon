df3a1b52a561bd71874a6e77c8308a29b2526f56
unknown <lukaszs@moomin.kainos.com>
2012-12-18 13:12:49 +0100

    added build dependencies for MongoDB and some others

--- project/Build.scala
+++ project/Build.scala
@@ -9,10 +9,18 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+        "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
+    	"com.novus" %% "salat-core" % "0.0.8-SNAPSHOT",
+    	"com.github.twitter" %  "bootstrap"  % "2.0.2"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here
+        resolvers ++= Seq(
+    		"repo.novus snaps" at "http://repo.novus.com/snapshots/",
+    		"casbah" at "https://oss.sonatype.org/content/groups/scala-tools/",
+    		"webjars" at "http://webjars.github.com/m2"
+    		)
     )
 
 }
