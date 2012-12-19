049174284e881b69747b9642a45b392dc7c976d0
lukasz-skibski <lukasz.skibski@gmail.com>
2012-12-19 15:43:30 +0100

    added mailer plugin dependencies, run play eclipsify

--- app/services/EmailService.scala
+++ app/services/EmailService.scala
@@ -7,6 +7,7 @@ import org.scala_tools.time.Imports._
 import models._
 import viewmodels._
 import com.novus.salat.dao.SalatDAO
+import com.typesafe.plugin._
 
 object EmailService {
 
--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,8 @@ object ApplicationBuild extends Build {
       // Add your project dependencies here,
         "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
     	"com.novus" %% "salat-core" % "0.0.8-SNAPSHOT",
-    	"com.github.twitter" %  "bootstrap"  % "2.0.2"
+    	"com.github.twitter" %  "bootstrap"  % "2.0.2",
+    	"com.typesafe" %% "play-plugins-mailer" % "2.0.4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
