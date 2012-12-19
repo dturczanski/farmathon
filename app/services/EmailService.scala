cf6874fc2560fa44293bfc3111cd87cb3d346e2d
lukasz-skibski <lukasz.skibski@gmail.com>
2012-12-19 16:00:59 +0100

    added working mail sending logic (will send email to @kainos.com domain)

--- app/services/EmailService.scala
+++ app/services/EmailService.scala
@@ -8,11 +8,23 @@ import models._
 import viewmodels._
 import com.novus.salat.dao.SalatDAO
 import com.typesafe.plugin._
+import play.api.Play.current
 
 object EmailService {
 
   def sendEmail(id: String) {
   
+	val enq: Enquiry = EnquiryService.get(id)
+    
+	val mail = use[MailerPlugin].email
+	mail.setSubject("Your application details")
+	mail.addRecipient(enq.firstName + " " + enq.lastName + " <" + enq.email + ">", enq.email)
+	mail.addFrom("Gdansk Hakaton Team <l.skibski@kainos.com>")
+	val contents: String = "Hello " + enq.firstName +",\n\nYour application details:\n\n" +  enq.firstName + " " +
+			enq.middleName + " " + enq.lastName + " " + enq.previousNames + "\n" + enq.email +
+			"\nApplication reference number: " + id + "\n\nRegards,\nFarmathon Team";
+	mail.send(contents)
+
   }
   
 }
