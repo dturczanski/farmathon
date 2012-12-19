529429b0f306355c4773c9273321f1e711ff8edf
Łukasz Młynik <requiemos@gmail.com>
2012-12-19 16:03:43 +0100

    Moved input to helper file.

--- app/services/EnquiryService.scala
+++ app/services/EnquiryService.scala
@@ -13,63 +13,63 @@ object EnquiryDAO extends SalatDAO[Enquiry, ObjectId](collection = MongoConnecti
 object EnquiryService {
 
   def all = EnquiryDAO.find(MongoDBObject.empty).toList
-  
+
   def outstanding = EnquiryDAO.find(MongoDBObject("status" -> "pending")).toList
 
   def get(id: String): Enquiry = {
     get(new ObjectId(id))
   }
-  
+
   def get(id: ObjectId): Enquiry = {
     val enq: Option[Enquiry] = EnquiryDAO.findOneById(id)
     enq.get
   }
-  
+
   def update(enquiryName: EnquiryName): String = {
-    if (enquiryName.id.equals("")){
-        val enq = Enquiry(None, enquiryName.firstName, enquiryName.middleName, enquiryName.lastName, enquiryName.previousNames, "", "draft")
-    	return EnquiryDAO.insert(enq).get.toString()
+    if (enquiryName.id.equals("")) {
+      val enq = Enquiry(None, enquiryName.firstName, enquiryName.middleName, enquiryName.lastName, enquiryName.previousNames, "", "draft")
+      return EnquiryDAO.insert(enq).get.toString()
     } else {
       val enq = get(enquiryName.id)
       val updated = enq.copy(firstName = enquiryName.firstName,
-        						middleName = enquiryName.middleName,
-        						lastName = enquiryName.lastName,
-        						previousNames = enquiryName.previousNames)
+        middleName = enquiryName.middleName,
+        lastName = enquiryName.lastName,
+        previousNames = enquiryName.previousNames)
 
-       EnquiryDAO.update(MongoDBObject("_id" -> updated._id), grater[Enquiry].asDBObject(updated), false, false)
-       return enquiryName.id;
+      EnquiryDAO.update(MongoDBObject("_id" -> updated._id), grater[Enquiry].asDBObject(updated), false, false)
+      return enquiryName.id;
     }
   }
-  
-  def update(enquiryEmail: EnquiryEmail) {
-      val enq = get(enquiryEmail.id)
-      val updated = enq.copy(email = enquiryEmail.email)
 
-       EnquiryDAO.update(MongoDBObject("_id" -> updated._id), grater[Enquiry].asDBObject(updated), false, false)
+  def update(enquiryEmail: EnquiryEmail) {
+    val enq = get(enquiryEmail.id)
+    val updated = enq.copy(email = enquiryEmail.email)
+
+    EnquiryDAO.update(MongoDBObject("_id" -> updated._id), grater[Enquiry].asDBObject(updated), false, false)
   }
-  
+
   def getName(id: String): EnquiryName = {
     val enq = get(id)
     val name = EnquiryName(id, enq.firstName, enq.middleName, enq.lastName, enq.previousNames)
     name
   }
-  
+
   def getEmail(id: String): EnquiryEmail = {
     val enq = get(id)
     val email = EnquiryEmail(id, enq.email)
     email
   }
-  
-  def updateStatus(id : String, newStatus : String) {
+
+  def updateStatus(id: String, newStatus: String) {
     val enquiry = get(id)
     EnquiryDAO.update(MongoDBObject("_id" -> enquiry._id), grater[Enquiry].asDBObject(enquiry.copy(status = newStatus)), false, false)
   }
 
   def findByIdAndSurname(id: String, surname: String) = {
-    var pattern = "[0-9a-f]{24}".r
-    id match {
-      case pattern(id) => EnquiryDAO.findOne(MongoDBObject("_id" -> new ObjectId(id), "lastName" -> surname))
-      case _ => None
+    try {
+      EnquiryDAO.findOne(MongoDBObject("_id" -> new ObjectId(id), "lastName" -> surname))
+    } catch {
+      case e: IllegalArgumentException => None
     }
   }
 
